0a579ab6c44e4180b171d5c95ea9a07d5f9c5cef
aemaaeng <haemin120@naver.com>
2023-02-14 00:18:43 +0900

    :memo: 스타트와 링크

--- Backtracking/14889.js
+++ Backtracking/14889.js
@@ -7,63 +7,50 @@ const N = Number(input.shift());
 const stats = input.map((el) => {
   return el.split(" ").map(Number);
 });
-
-const half = Math.floor(N / 2);
-const start = [];
-let link = [];
+const half = N / 2;
 let min = Infinity;
-
 const visited = new Array(N).fill(false);
 
-function dfs(v) {
-  // start 배열이 절반의 크기와 같을 때
-  if (v === half) {
+function dfs(count, v) {
+  // 딱 절반 나뉘었을 때
+  if (count === half) {
+    const start = [];
+    const link = [];
     let startSum = 0;
     let linkSum = 0;
 
-    // start 배열을 탐색하면서 없는 건 link에 넣는다
-    for (let i = 1; i < N + 1; i++) {
-      if (start.indexOf(i) === -1) {
+    // visited에 있는 건 start, 없는 건 link에
+    for (let i = 0; i < N; i++) {
+      if (visited[i]) {
+        start.push(i);
+      } else {
         link.push(i);
       }
     }
 
-    // console.log("start: ", start);
-    // console.log("link: ", link);
-    // console.log("----------------");
-
     // 합 구하기
-    for (let i = 0; i < half - 1; i++) {
+    for (let i = 0; i < half; i++) {
       for (let j = i + 1; j < half; j++) {
-        startSum +=
-          stats[start[i] - 1][start[j] - 1] + stats[start[j] - 1][start[i] - 1];
-        linkSum +=
-          stats[link[i] - 1][link[j] - 1] + stats[link[j] - 1][link[i] - 1];
+        startSum += stats[start[i]][start[j]] + stats[start[j]][start[i]];
+        linkSum += stats[link[i]][link[j]] + stats[link[j]][link[i]];
       }
     }
 
-    // min보다 작으면 할당
+    // min보다 작으면 업데이트
     let diff = Math.abs(startSum - linkSum);
     if (min > diff) {
       min = diff;
     }
-
-    link = [];
     return;
   }
 
   // 수열 만드는 백트래킹
-  for (let i = 0; i < N; i++) {
-    if (visited[i]) continue;
-
+  for (let i = v; i < N; i++) {
     visited[i] = true;
-    start.push(i + 1);
-    dfs(v + 1);
-
-    start.pop();
+    dfs(count + 1, i + 1);
     visited[i] = false;
   }
 }
 
-dfs(0);
+dfs(0, 0);
 console.log(min);
