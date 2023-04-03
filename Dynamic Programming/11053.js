4fc90ec85725e3675bdc3a8d4e211b04dd3d2be3
aemaaeng <haemin120@naver.com>
2023-04-03 14:56:08 +0900

    :memo: 가장 긴 증가하는 부분수열

---
+++ Dynamic Programming/11053.js
@@ -0,0 +1,21 @@
+// 가장 긴 증가하는 부분수열
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = Number(input[0]);
+const A = input[1].split(" ").map((el) => Number(el));
+
+const D = new Array(N).fill(0);
+
+for (let i = 0; i < N; i++) {
+  let max = 0;
+  for (let j = 0; j < i; j++) {
+    if (A[i] > A[j] && D[j] > max) {
+      max = D[j];
+    }
+  }
+  D[i] = max + 1;
+}
+
+console.log(Math.max(...D));
