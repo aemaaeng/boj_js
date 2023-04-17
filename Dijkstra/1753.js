2a6338d59012ca7daae8ee6325980fbb1067ee4a
aemaaeng <haemin120@naver.com>
2023-04-17 23:49:53 +0900

    :memo: 최단경로 - 최소 힙 풀이

--- Dijkstra/1753.js
+++ Dijkstra/1753.js
@@ -1,122 +1,90 @@
-// 우선순위 큐
-class Node {
-  constructor(val, priority) {
-    this.val = val;
-    this.priority = priority;
-  }
-}
-
-class PriorityQueue {
+// 최소 힙
+class MinHeap {
   constructor() {
-    this.values = [];
+    this.heap = [null];
   }
-  enqueue(val, priority) {
-    let newNode = new Node(val, priority);
-    this.values.push(newNode);
-    this.bubbleUp();
+  getMin() {
+    return this.heap[1] ? this.heap[1] : null;
   }
-  bubbleUp() {
-    let idx = this.values.length - 1;
-    const element = this.values[idx];
-    while (idx > 0) {
-      let parentIdx = Math.floor((idx - 1) / 2);
-      let parent = this.values[parentIdx];
-      if (element.priority >= parent.priority) break;
-      this.values[parentIdx] = element;
-      this.values[idx] = parent;
-      idx = parentIdx;
+  size() {
+    return this.heap.length - 1;
+  }
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+  push(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = Math.floor(curIdx / 2);
+
+    while (curIdx > 1 && this.heap[parIdx].w > this.heap[curIdx].w) {
+      this.swap(curIdx, parIdx);
+      curIdx = parIdx;
+      parIdx = Math.floor(curIdx / 2);
     }
   }
-  dequeue() {
-    const min = this.values[0];
-    const end = this.values.pop();
-    if (this.values.length > 0) {
-      this.values[0] = end;
-      this.sinkDown();
+  pop() {
+    const min = this.heap[1];
+    if (this.heap.length <= 2) {
+      this.heap = [null];
+    } else {
+      this.heap[1] = this.heap.pop();
     }
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.heap[leftIdx]) return min;
+    if (!this.heap[rightIdx]) {
+      if (this.heap[leftIdx].w < this.heap[curIdx].w) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while (
+      leftIdx < this.size() &&
+      (this.heap[leftIdx].w < this.heap[curIdx].w ||
+        this.heap[rightIdx].w < this.heap[curIdx].w)
+    ) {
+      const minIdx =
+        this.heap[leftIdx].w > this.heap[rightIdx].w ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+
     return min;
   }
-  sinkDown() {
-    let idx = 0;
-    const length = this.values.length;
-    const element = this.values[0];
-    while (true) {
-      let leftChildIdx = 2 * idx + 1;
-      let rightChildIdx = 2 * idx + 2;
-      let leftChild, rightChild;
-      let swap = null;
-
-      if (leftChildIdx < length) {
-        leftChild = this.values[leftChildIdx];
-        if (leftChild.priority < element.priority) {
-          swap = leftChildIdx;
-        }
-      }
-      if (rightChildIdx < length) {
-        rightChild = this.values[rightChildIdx];
-        if (
-          (swap === null && rightChild.priority < element.priority) ||
-          (swap !== null && rightChild.priority < leftChild.priority)
-        ) {
-          swap = rightChildIdx;
-        }
-      }
-      if (swap === null) break;
-      this.values[idx] = this.values[swap];
-      this.values[swap] = element;
-      idx = swap;
-    }
-  }
 }
 
-// 가중치 그래프
-class WeightedGraph {
-  constructor() {
-    this.adjacencyList = {};
-  }
-  // add vertex
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-  }
-  // add edge -> with weight
-  addEdge(vertex1, vertex2, weight) {
-    this.adjacencyList[vertex1].push({ node: vertex2, weight });
-  }
-  Dijkstra(start, finish) {
-    const nodes = new PriorityQueue();
-    const distances = {};
-    const previous = {};
-    let smallest;
+function dijkstra(start) {
+  dist[start] = 0;
+  visited[start] = true;
 
-    // build up initial state
-    for (let vertex in this.adjacencyList) {
-      if (vertex === start) {
-        distances[vertex] = 0;
-        nodes.enqueue(vertex, 0);
-      } else {
-        distances[vertex] = Infinity;
-        nodes.enqueue(vertex, Infinity);
-      }
-      previous[vertex] = null;
-    }
+  const minHeap = new MinHeap();
+  minHeap.push({
+    vertex: start,
+    w: 0,
+  });
 
-    // as long as there is something to visit
-    while (nodes.values.length) {
-      smallest = nodes.dequeue().val;
-      if (smallest === finish) {
-        return distances[finish] === Infinity ? "INF" : distances[finish];
-      }
-      if (smallest || distances[smallest] !== Infinity) {
-        for (let neighbor in this.adjacencyList[smallest]) {
-          let nextNode = this.adjacencyList[smallest][neighbor];
-          let candidate = distances[smallest] + nextNode.weight;
-          if (candidate < distances[nextNode.node]) {
-            // update new smallest distance to neighbor
-            distances[nextNode.node] = candidate;
-            previous[nextNode.node] = smallest;
-            nodes.enqueue(nextNode.node, candidate);
-          }
-        }
+  while (minHeap.size() > 0) {
+    const { vertex, w } = minHeap.pop();
+    visited[vertex] = true;
+
+    for (let i = 0; i < graph[vertex].length; i++) {
+      const cur = graph[vertex][i];
+      const next = cur[0];
+      const nextWeight = cur[1];
+
+      if (visited[next]) continue;
+
+      if (dist[next] > dist[vertex] + nextWeight) {
+        dist[next] = dist[vertex] + nextWeight;
+
+        minHeap.push({ vertex: next, w: dist[next] });
       }
     }
   }
@@ -126,25 +94,23 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const graph = new WeightedGraph();
-
 const [V, E] = input[0].split(" ").map(Number);
-const startNode = input[1];
+const graph = Array.from({ length: V + 1 }, () => []);
+let dist = new Array(V + 1).fill(Infinity);
+const visited = new Array(V + 1).fill(false);
+const startNode = Number(input[1]);
 
 const answer = [];
 
-for (let i = 1; i <= V; i++) {
-  graph.addVertex(`${i}`);
-}
-
 for (let info of input.slice(2)) {
-  const [u, v, w] = info.split(" ");
-  graph.addEdge(u, v, +w);
+  const [u, v, w] = info.split(" ").map(Number);
+  graph[u].push([v, w]);
 }
 
-// 1부터 V까지 반복문을 돌며 Dijkstra 알고리즘을 수행한다.
+dijkstra(startNode);
+
 for (let i = 1; i <= V; i++) {
-  answer.push(graph.Dijkstra(`${startNode}`, `${i}`));
+  dist[i] === Infinity ? answer.push("INF") : answer.push(dist[i]);
 }
 
 console.log(answer.join("\n"));
