fa4119a340f0d58bdad2e209a180102110dbc926
aemaaeng <haemin120@naver.com>
2023-04-23 14:05:53 +0900

    :memo: 차이를 최대로 - 2회차 풀이

--- Backtracking/10819.js
+++ Backtracking/10819.js
@@ -3,35 +3,38 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(input[0]);
-const M = input[1].split(" ").map(Number);
+const N = +input[0];
+const arr = input[1].split(" ").map(Number);
 
-let max = 0;
-let tmp = [];
-let visited = new Array(N + 1).fill(0);
-
-function dfs(cnt) {
-  if (cnt === N) {
-    if (max < sum(tmp)) max = sum(tmp);
-  } else {
-    for (let i = 0; i < N; i++) {
-      if (!visited[i]) {
-        visited[i] = 1;
-        tmp.push(M[i]);
-        dfs(cnt + 1);
-        tmp.pop();
-        visited[i] = 0;
-      }
-    }
+// 합 연산 수행 함수
+function sum(arr) {
+  let answer = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    answer += Math.abs(arr[i] - arr[i + 1]);
   }
+  return answer;
 }
 
-function sum(arr) {
-  let sum = 0;
-  for (let i = 0; i < N - 1; i++) {
-    sum += Math.abs(arr[i] - arr[i + 1]);
+const visited = new Array(N + 1).fill(false);
+// 임시 배열을 하나 만들어서 거기에 백트래킹의 결과 배열을 담는다!
+let tmp = [];
+let max = 0;
+
+function dfs(cnt) {
+  if (N === cnt) {
+    // base case
+    if (max < sum(tmp)) max = sum(tmp);
+  } else {
+    // 백트래킹 -> 방문처리 후 dfs를 수행하고 방문처리 해제
+    for (let i = 0; i < N; i++) {
+      if (visited[i]) continue;
+      visited[i] = true;
+      tmp.push(arr[i]);
+      dfs(cnt + 1);
+      tmp.pop();
+      visited[i] = false;
+    }
   }
-  return sum;
 }
 
 dfs(0);
