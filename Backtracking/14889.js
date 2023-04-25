3abc689046f412af4cf0df5ff3cdee64cac40cd2
aemaaeng <haemin120@naver.com>
2023-04-25 17:05:09 +0900

    :memo: 스타트와 링크 - 2회차 풀이
    backtracking 함수에 인자를 두 개 넘겨주는 것이 포인트였다

--- Backtracking/14889.js
+++ Backtracking/14889.js
@@ -3,23 +3,30 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(input.shift());
-const stats = input.map((el) => {
-  return el.split(" ").map(Number);
-});
-const half = N / 2;
+const N = Number(input[0]);
+const arr = input.slice(1).map((el) => el.split(" ").map(Number));
+
 let min = Infinity;
 const visited = new Array(N).fill(false);
 
-function dfs(count, v) {
-  // 딱 절반 나뉘었을 때
-  if (count === half) {
-    const start = [];
-    const link = [];
-    let startSum = 0;
-    let linkSum = 0;
+function diff(arr1, arr2) {
+  let startSum = 0;
+  let linkSum = 0;
 
-    // visited에 있는 건 start, 없는 건 link에
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = i + 1; j < arr1.length; j++) {
+      startSum += arr[arr1[i]][arr1[j]] + arr[arr1[j]][arr1[i]];
+      linkSum += arr[arr2[i]][arr2[j]] + arr[arr2[j]][arr2[i]];
+    }
+  }
+
+  return Math.abs(startSum - linkSum);
+}
+
+function backtracking(cnt, v) {
+  if (cnt === N / 2) {
+    let start = [];
+    let link = [];
     for (let i = 0; i < N; i++) {
       if (visited[i]) {
         start.push(i);
@@ -27,30 +34,17 @@ function dfs(count, v) {
         link.push(i);
       }
     }
-
-    // 합 구하기
-    for (let i = 0; i < half; i++) {
-      for (let j = i + 1; j < half; j++) {
-        startSum += stats[start[i]][start[j]] + stats[start[j]][start[i]];
-        linkSum += stats[link[i]][link[j]] + stats[link[j]][link[i]];
-      }
-    }
-
-    // min보다 작으면 업데이트
-    let diff = Math.abs(startSum - linkSum);
-    if (min > diff) {
-      min = diff;
-    }
+    if (diff(start, link) < min) min = diff(start, link);
+    cnt = 0;
     return;
   }
 
-  // 수열 만드는 백트래킹
   for (let i = v; i < N; i++) {
     visited[i] = true;
-    dfs(count + 1, i + 1);
+    backtracking(cnt + 1, i + 1);
     visited[i] = false;
   }
 }
 
-dfs(0, 0);
+backtracking(0, 0);
 console.log(min);
