aa464a0fc13c5f932c0b3c6e2a8e1f52268036fc
aemaaeng <haemin120@naver.com>
2023-05-02 16:14:24 +0900

    :memo: 안전영역 dfs - 2회차 풀이

--- Graph/2468.js
+++ Graph/2468.js
@@ -1,48 +1,42 @@
-// 안전영역
+// 안전영역 - dfs
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(input.shift());
-const swamp = input.map((el) => el.split(" ").map(Number));
+const N = Number(input[0]);
+const arr = input.slice(1).map((el) => el.split(" ").map(Number));
 
-const dx = [0, 0, 1, -1];
-const dy = [1, -1, 0, 0];
+let max = 0;
 
-let maxHeight = 0;
+// 최대 높이 구하기
+arr.forEach((row) => {
+  row.forEach((el) => {
+    max = Math.max(max, el);
+  });
+});
 
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (swamp[i][j] > maxHeight) maxHeight = swamp[i][j];
-  }
-}
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
 
+// 주어진 높이에 해당하는 안전영역의 크기를 리턴하는 함수
 function solution(height) {
-  const visited = Array.from(new Array(N + 1), () => new Array(N + 1));
   let cnt = 0;
+  const visited = Array.from(new Array(N + 1), () =>
+    new Array(N + 1).fill(false)
+  );
 
-  // 잠기지 않는 영역을 하나씩 세는 dfs
   function dfs(x, y, h) {
     if (visited[x][y]) return;
     visited[x][y] = true;
     visitedCoords[[x, y]] = true;
-    if (swamp[x][y] > h) cnt++;
+    if (arr[x][y] > h) cnt++;
 
-    // 상하좌우 탐색
     for (let i = 0; i < 4; i++) {
       let nx = x + dx[i];
       let ny = y + dy[i];
 
-      if (
-        nx < N &&
-        ny < N &&
-        nx >= 0 &&
-        ny >= 0 &&
-        !visited[nx][ny] &&
-        swamp[nx][ny] > h
-      ) {
-        dfs(nx, ny, h);
-      }
+      if (nx >= N || ny >= N || nx < 0 || ny < 0) continue;
+      if (!visited[nx][ny] && arr[nx][ny] > h) dfs(nx, ny, h);
     }
 
     return cnt;
@@ -53,7 +47,7 @@ function solution(height) {
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (swamp[i][j] > height && !visitedCoords[[i, j]]) {
+      if (arr[i][j] > height && !visitedCoords[[i, j]]) {
         safeZone.push(dfs(i, j, height));
         cnt = 0;
       }
@@ -63,8 +57,11 @@ function solution(height) {
   return safeZone.length;
 }
 
-const answer = [];
-for (let i = 0; i < maxHeight; i++) {
-  answer.push(solution(i));
+let answer = 0;
+
+// 최솟값에서 시작해 최댓값까지 비를 내려보기 (for)
+for (let i = 0; i < max; i++) {
+  if (solution(i) > answer) answer = solution(i);
 }
-console.log(Math.max(...answer));
+
+console.log(answer);
