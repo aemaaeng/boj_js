19f5a93e50cd0cf14ecd138ca21fd2b237c3ebb1
aemaaeng <haemin120@naver.com>
2023-05-04 15:31:58 +0900

    :memo: 알파벳 - 2회차 풀이

--- Graph/1987.js
+++ Graph/1987.js
@@ -3,32 +3,32 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [R, C] = input.shift().split(" ").map(Number);
-const board = input.map((row) => row.split(""));
+const [R, C] = input[0].split(" ").map(Number);
+const board = input.slice(1).map((el) => el.split(""));
 
-let ans = 0;
-const dx = [0, 0, 1, -1];
-const dy = [1, -1, 0, 0];
+const alpha = new Array(26).fill(false);
 
-const visited = new Array(26).fill(false);
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+let max = 0;
 
+// dfs
 function dfs(x, y, cnt) {
-  if (ans < cnt) ans = cnt;
+  if (max < cnt) max = cnt;
 
   for (let i = 0; i < 4; i++) {
-    let nx = x + dx[i];
-    let ny = y + dy[i];
+    const nx = x + dx[i];
+    const ny = y + dy[i];
 
-    if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
-      if (!visited[board[nx][ny].charCodeAt() - 65]) {
-        visited[board[nx][ny].charCodeAt() - 65] = true;
-        dfs(nx, ny, cnt + 1);
-        visited[board[nx][ny].charCodeAt() - 65] = false;
-      }
+    if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+    if (!alpha[board[nx][ny].charCodeAt() - 65]) {
+      alpha[board[nx][ny].charCodeAt() - 65] = true;
+      dfs(nx, ny, cnt + 1);
+      alpha[board[nx][ny].charCodeAt() - 65] = false;
     }
   }
 }
 
-visited[board[0][0].charCodeAt() - 65] = true;
+alpha[board[0][0].charCodeAt() - 65] = true;
 dfs(0, 0, 1);
-console.log(ans);
+console.log(max);
