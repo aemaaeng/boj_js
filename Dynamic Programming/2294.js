659bea08ef3a7df706689fd2197abe9eca8d4a53
aemaaeng <haemin120@naver.com>
2023-05-08 21:50:53 +0900

    :memo: 동전 2 - 2회차 풀이

--- Dynamic Programming/2294.js
+++ Dynamic Programming/2294.js
@@ -1,14 +1,15 @@
+// 동전 2
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [N, K] = input[0].split(" ").map(Number);
-// 중복 제거 및 정렬
+// 오름차순으로 정렬, 중복 제거
 const coins = [
   ...new Set(
     input
       .slice(1)
-      .map((el) => Number(el))
+      .map(Number)
       .sort((a, b) => a - b)
   ),
 ];
