9cca81e749ef892ab9880676dc11a17292cb5aae
aemaaeng <haemin120@naver.com>
2023-05-09 16:34:48 +0900

    :memo: 이친수 - 2회차 풀이

--- Dynamic Programming/2193.js
+++ Dynamic Programming/2193.js
@@ -1,15 +1,15 @@
 // 이친수
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
-let N = parseInt(fs.readFileSync(filePath).toString().trim());
+const N = Number(fs.readFileSync(filePath).toString().trim());
 
-const memo = new Array(N + 1).fill(0);
-memo[1] = 1;
-memo[2] = 1;
+const D = [];
+D[0] = 0;
+D[1] = 1;
+D[2] = 1;
 
-for (let i = 3; i < memo.length; i++) {
-  // 수의 범위를 초과하는 경우가 있기 때문에 BigInt 자료형을 써야 함.
-  memo[i] = BigInt(memo[i - 1]) + BigInt(memo[i - 2]);
+for (let i = 3; i <= N; i++) {
+  D[i] = BigInt(D[i - 1]) + BigInt(D[i - 2]);
 }
 
-console.log(String(memo[N]));
+console.log(String(D[N]));
