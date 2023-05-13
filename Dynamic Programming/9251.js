7423f57ccaf33bab0a2c8a923fe03e4bda28f32a
aemaaeng <haemin120@naver.com>
2023-05-13 11:47:45 +0900

    :memo: LCS - 2회차 풀이

--- Dynamic Programming/9251.js
+++ Dynamic Programming/9251.js
@@ -1,17 +1,18 @@
-// LCS
+// LCS Longest Common Subsequence
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [A, B] = input;
-const N = A.length;
-const M = B.length;
+const [str1, str2] = input;
+const N = str1.length;
+const M = str2.length;
 
+// 가장 긴 부분수열의 길이
 const D = Array.from(new Array(N + 1), () => new Array(M + 1).fill(0));
 
 for (let i = 1; i <= N; i++) {
   for (let j = 1; j <= M; j++) {
-    if (A[i - 1] === B[j - 1]) {
+    if (str1[i - 1] === str2[j - 1]) {
       D[i][j] = D[i - 1][j - 1] + 1;
     } else {
       D[i][j] = Math.max(D[i][j - 1], D[i - 1][j]);
@@ -19,4 +20,14 @@ for (let i = 1; i <= N; i++) {
   }
 }
 
+// console.log(D);
+// [
+//   [0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 1, 1, 1, 1, 1],
+//   [0, 1, 1, 1, 2, 2, 2],
+//   [0, 1, 2, 2, 2, 3, 3],
+//   [0, 1, 2, 2, 2, 3, 3],
+//   [0, 1, 2, 2, 2, 3, 4],
+//   [0, 1, 2, 3, 3, 3, 4],
+// ];
 console.log(D[N][M]);
