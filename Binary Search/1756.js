a32953565c8ba89ed4b6007d7080c6caa47f6eb4
aemaaeng <haemin120@naver.com>
2023-05-16 18:25:28 +0900

    :memo: 피자 굽기

--- Binary Search/1756.js
+++ Binary Search/1756.js
@@ -7,6 +7,7 @@ const [D, N] = input[0].split(" ").map(Number);
 const oven = input[1].split(" ").map(Number);
 const pizza = input[2].split(" ").map(Number);
 let limit = D;
+
 // 반복문 처리를 먼저
 // 이전 깊이의 지름이 자기 자신보다 작다면, 그 작은 곳에게 맞춘다 -> 어차피 막혀서 못 들어가기 때문
 // 5 6 4 3 6 2 3 -> 5 5 4 3 3 2 2
@@ -16,19 +17,19 @@ for (let i = 1; i < D; i++) {
 
 for (let i = 0; i < N; i++) {
   let left = 0;
-  let right = D - 1;
-  limit = limit - 1;
+  let right = limit - 1;
 
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    // console.log(mid);
+    let mid = Math.floor(left + (right - left) / 2);
     if (oven[mid] >= pizza[i]) {
       left = mid + 1;
     } else {
       right = mid - 1;
     }
-    limit = right;
   }
+  limit = right;
 }
 
 console.log(limit >= 0 ? limit + 1 : 0);
+
+// 참고: https://github.com/flip1945/TIL/blob/1f49203c4a4bc45100d3af576dbb7a86641d12d5/Algorithm/Binary%20Search/BOJ_%ED%94%BC%EC%9E%90%20%EA%B5%BD%EA%B8%B0.md?plain=1#L1
