0e5efeb0608b11b47be96ec9cd2ff73a309d0415
aemaaeng <haemin120@naver.com>
2023-05-20 23:37:12 +0900

    :memo: 개똥벌레 - 2회차 풀이

--- Prefix Sum/3020.js
+++ Prefix Sum/3020.js
@@ -1,3 +1,4 @@
+// 개똥벌레
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -6,14 +7,18 @@ const [N, H] = input.shift().split(" ").map(Number);
 const bottom = new Array(H + 1).fill(0);
 const top = new Array(H + 1).fill(0);
 
+// bottom, top 배열에 각 높이가 등장한 횟수를 저장한다
 input.forEach((el, idx) => {
-  idx % 2 === 0 ? bottom[Number(el)]++ : top[H - Number(el) + 1]++;
+  idx % 2 === 0 ? (bottom[Number(el)] += 1) : (top[H - Number(el) + 1] += 1);
 });
 
 for (let i = 1; i <= H; i++) {
   top[i] += top[i - 1];
   bottom[H - i] += bottom[H - i + 1];
 }
+
+// console.log(bottom);
+// console.log(top);
 
 let min = Infinity;
 let cnt = 0;
@@ -22,8 +27,10 @@ for (let i = 1; i <= H; i++) {
     cnt = 1;
     min = top[i] + bottom[i];
   } else if (top[i] + bottom[i] === min) {
-    cnt++;
+    cnt += 1;
   }
 }
 
 console.log(min, cnt);
+
+// 참고: https://koguri.tistory.com/111
