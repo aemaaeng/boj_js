d23ff8b49f9442ea9b2e5a6f2e8ae92ea87a16b0
aemaaeng <haemin120@naver.com>
2023-05-23 22:26:26 +0900

    :memo: 욕심쟁이 판다 - 2회차 풀이

--- Dynamic Programming/1937.js
+++ Dynamic Programming/1937.js
@@ -1,3 +1,4 @@
+// 욕심쟁이 판다
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -5,32 +6,35 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = Number(input[0]);
 const forest = input.slice(1).map((el) => el.split(" ").map(Number));
 
-const visited = Array.from(new Array(N), () => new Array(N).fill(false));
-const dx = [0, 0, -1, 1];
-const dy = [-1, 1, 0, 0];
-let answer = 0;
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+const visited = Array.from(new Array(N + 1), () => new Array(N + 1).fill(0));
 
 function dfs(x, y) {
-  if (visited[x][y] === false) {
+  if (!visited[x][y]) {
     visited[x][y] = 1;
     let temp = 0;
     for (let i = 0; i < 4; i++) {
-      const cx = dx[i] + x;
-      const cy = dy[i] + y;
+      const nx = x + dx[i];
+      const ny = y + dy[i];
 
-      if (cx < 0 || cy < 0 || cx >= N || cy >= N) continue;
-      if (forest[x][y] > forest[cx][cy]) temp = Math.max(temp, dfs(cx, cy));
+      if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+      if (forest[x][y] < forest[nx][ny]) {
+        temp = Math.max(temp, dfs(nx, ny));
+      }
     }
     visited[x][y] += temp;
   }
+
   return visited[x][y];
 }
 
+let answer = 0;
+
+// 각 지점마다 dfs를 돌려본다
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (visited[i][j] === false) {
-      answer = Math.max(answer, dfs(i, j));
-    }
+    answer = Math.max(answer, dfs(i, j));
   }
 }
 
