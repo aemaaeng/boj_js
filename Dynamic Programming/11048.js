3072bb2a2a84e6b8105c4e264177f72f26bd88da
aemaaeng <haemin120@naver.com>
2023-05-25 11:59:07 +0900

    :memo: 이동하기 - 3회차 풀이

--- Dynamic Programming/11048.js
+++ Dynamic Programming/11048.js
@@ -7,6 +7,8 @@ const [N, M] = input[0].split(" ").map(Number);
 const maze = input.slice(1).map((el) => el.split(" ").map(Number));
 const D = Array.from(new Array(N), () => new Array(M).fill(0));
 
+// 아래, 오른쪽, 대각선 아래로만 이동 가능
+// 위, 왼쪽, 대각선 위 요소들 중 큰 것을 취하며 테이블을 채워나간다
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) {
     const top = i > 0 ? D[i - 1][j] : 0;
