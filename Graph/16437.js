3939416f7f7b73411a943d4aefd447bdfde8de19
aemaaeng <haemin120@naver.com>
2023-05-27 22:17:08 +0900

    :memo: 양 구출작전 - 2회차 풀이

--- Graph/16437.js
+++ Graph/16437.js
@@ -1,5 +1,4 @@
-// 양 구출 작전
-// 참고:  https://kagrin97-blog.vercel.app/algorithm/16437-%EC%96%91%20%EA%B5%AC%EC%B6%9C%20%EC%9E%91%EC%A0%84
+// 양 구출작전
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -7,33 +6,32 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = Number(input.shift());
 const islands = input.map((el) => el.split(" "));
 
-// 양의 수를 리턴하는 dfs
-function dfs(curIdx) {
-  let sheepCnt = 0;
-  for (let i of tree[curIdx]) {
-    sheepCnt += dfs(i);
+const islandInfo = [[], [0, 0]]; // 섬에 어떤 동물이 몇 마리 살고 있는지 담은 배열
+const tree = Array.from(new Array(N + 1), () => new Array().fill([])); // 각 섬의 관계를 트리 자료구조로 표현
+
+function dfs(x) {
+  let cnt = 0;
+  // 재귀
+  for (let i of tree[x]) {
+    cnt += dfs(i);
   }
 
-  if (islandInfo[curIdx][0] === "W") {
-    sheepCnt -= islandInfo[curIdx][1];
-    if (sheepCnt < 0) sheepCnt = 0;
+  // tree에서 dfs를 돌고 s이면 더하고 w이면 뺀다
+  if (islandInfo[x][0] === "W") {
+    cnt -= islandInfo[x][1];
+    if (cnt < 0) cnt = 0;
   } else {
-    sheepCnt += islandInfo[curIdx][1];
+    cnt += islandInfo[x][1];
   }
 
-  return sheepCnt;
+  return cnt;
 }
 
-// 실제 섬 번호와 맞추기 위한 더미 데이터 두 개를 미리 넣어놓는다.
-let islandInfo = [[], [0, 0]];
-// 섬 간의 연결 정보를 담을 tree 배열
-let tree = Array.from(new Array(N + 1), () => new Array().fill([]));
-
-islands.forEach((island, idx) => {
-  const [t, a, p] = island;
+islands.forEach((el, idx) => {
+  const [t, a, p] = el;
   islandInfo.push([t, Number(a)]);
   tree[Number(p)].push(idx + 2);
 });
 
-// 1번 섬부터 dfs 시작
+// 뿌리는 1번 섬, 1번 섬을 시작점으로 dfs를 돈다
 console.log(dfs(1));
