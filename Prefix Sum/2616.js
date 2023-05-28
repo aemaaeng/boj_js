d47af50318d8c03e7159ca80e5e43d20ddfc5a56
aemaaeng <haemin120@naver.com>
2023-05-28 18:10:15 +0900

    :memo: 소형기관차 - 2회차 풀이

--- Prefix Sum/2616.js
+++ Prefix Sum/2616.js
@@ -1,25 +1,28 @@
+// 소형기관차
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const N = Number(input[0]);
-const passengers = input[1].split(" ").map((el) => Number(el));
+const passengers = input[1].split(" ").map(Number);
 const limit = Number(input[2]);
 
 const P = [0];
-for (let i = 1; i < N + 1; i++) {
+
+// prefix sum
+for (let i = 1; i <= N; i++) {
   P[i] = P[i - 1] + passengers[i - 1];
 }
 
-const memo = Array.from(new Array(4), () => new Array(N + 1).fill(0));
+const D = Array.from(new Array(4), () => new Array(N + 1).fill(0));
 
+// 소형기관차는 무조건 3대만 있다
 for (let i = 1; i <= 3; i++) {
+  // 기관차 첫 번째 객차부터 살펴보면서 테이블을 채워나간다
   for (let j = i * limit; j <= N; j++) {
-    memo[i][j] = Math.max(
-      memo[i][j - 1],
-      memo[i - 1][j - limit] + (P[j] - P[j - limit])
-    );
+    // prefix sum 이용하기
+    D[i][j] = Math.max(D[i][j - 1], D[i - 1][j - limit] + P[j] - P[j - limit]);
   }
 }
 
-console.log(memo[3][N]);
+console.log(D[3][N]);
