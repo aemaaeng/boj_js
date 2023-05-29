e944c63a9e9c9cb828de0cdc466c025a2b022059
aemaaeng <haemin120@naver.com>
2023-05-29 18:44:14 +0900

    :memo: 피자 굽기 - 2회차 풀이

--- Binary Search/1756.js
+++ Binary Search/1756.js
@@ -8,8 +8,6 @@ const oven = input[1].split(" ").map(Number);
 const pizza = input[2].split(" ").map(Number);
 let limit = D;
 
-// 반복문 처리를 먼저
-// 이전 깊이의 지름이 자기 자신보다 작다면, 그 작은 곳에게 맞춘다 -> 어차피 막혀서 못 들어가기 때문
 // 5 6 4 3 6 2 3 -> 5 5 4 3 3 2 2
 for (let i = 1; i < D; i++) {
   if (oven[i - 1] < oven[i]) oven[i] = oven[i - 1];
@@ -21,9 +19,11 @@ for (let i = 0; i < N; i++) {
 
   while (left <= right) {
     let mid = Math.floor(left + (right - left) / 2);
+
+    // oven 배열이 내림차순으로 정렬되어 있으므로 일반 이분탐색과 반대로 생각해야 함
     if (oven[mid] >= pizza[i]) {
       left = mid + 1;
-    } else {
+    } else if (oven[mid] < pizza[i]) {
       right = mid - 1;
     }
   }
@@ -31,5 +31,3 @@ for (let i = 0; i < N; i++) {
 }
 
 console.log(limit >= 0 ? limit + 1 : 0);
-
-// 참고: https://github.com/flip1945/TIL/blob/1f49203c4a4bc45100d3af576dbb7a86641d12d5/Algorithm/Binary%20Search/BOJ_%ED%94%BC%EC%9E%90%20%EA%B5%BD%EA%B8%B0.md?plain=1#L1
