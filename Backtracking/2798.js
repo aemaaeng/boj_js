2df8a996e80e3bf6deb40d420021b12924000bb1
aemaaeng <haemin120@naver.com>
2023-05-30 16:16:26 +0900

    :memo: 블랙잭 - 3회차 풀이

--- Backtracking/2798.js
+++ Backtracking/2798.js
@@ -4,29 +4,30 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [N, M] = input[0].split(" ").map(Number);
-const arr = input[1].split(" ").map(Number);
+const cards = input[1].split(" ").map(Number);
 
-let tmp = 0;
-let answer = 0;
-const visited = new Array(N).fill(false);
+let max = 0;
+const visited = new Array(N + 1).fill(false);
+const picked = [];
+
+function add(arr) {
+  return arr.reduce((acc, cur) => acc + cur);
+}
 
 function backtracking(cnt) {
   if (cnt === 3) {
-    if (answer < tmp && tmp <= M) {
-      answer = tmp;
-    }
-    cnt = 0;
+    if (max < add(picked) && add(picked) <= M) max = add(picked);
   } else {
     for (let i = 0; i < N; i++) {
       if (visited[i]) continue;
       visited[i] = true;
-      tmp += arr[i];
+      picked.push(cards[i]);
       backtracking(cnt + 1);
-      tmp -= arr[i];
+      picked.pop(cards[i]);
       visited[i] = false;
     }
   }
 }
 
 backtracking(0);
-console.log(answer);
+console.log(max);
