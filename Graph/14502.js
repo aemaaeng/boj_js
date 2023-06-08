3bab12d697871bfc2f8a2c8a024f245df5cb98dd
aemaaeng <haemin120@naver.com>
2023-06-08 16:55:17 +0900

    :memo: 연구소 - 3회차 풀이

--- Graph/14502.js
+++ Graph/14502.js
@@ -4,15 +4,16 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
-const map = input.map((el) => el.split(" ").map(Number));
+const field = input.map((el) => el.split(" ").map(Number));
 
 let answer = 0;
 
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+
 function bfs(arr) {
   let cnt = 0;
-  let queue = [];
-  const dx = [0, 0, -1, 1];
-  const dy = [-1, 1, 0, 0];
+  const queue = [];
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
@@ -20,47 +21,45 @@ function bfs(arr) {
     }
   }
 
-  // 바이러스 퍼트리기
   while (queue.length) {
     const [x, y] = queue.shift();
-
     for (let i = 0; i < 4; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
+      const nx = dx[i] + x;
+      const ny = dy[i] + y;
 
-      if (nx >= 0 && ny >= 0 && nx < N && ny < M && arr[nx][ny] === 0) {
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+      if (arr[nx][ny] === 0) {
         arr[nx][ny] = 2;
         queue.push([nx, ny]);
       }
     }
   }
 
-  // 안전영역 세기
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      if (arr[i][j] === 0) cnt++;
+      if (arr[i][j] === 0) cnt += 1;
     }
   }
 
   return cnt;
 }
 
+// 벽 세우는 백트래킹
 function backtracking(cnt) {
   if (cnt === 3) {
-    let copied = map.map((el) => [...el]);
-    let safeZone = bfs(copied);
+    // bfs 실행 후 값 업데이트
+    let copied = field.map((el) => [...el]);
+    let res = bfs(copied);
 
-    answer = Math.max(safeZone, answer);
+    answer = Math.max(answer, res);
     return;
   }
-
-  // 벽 세우기
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      if (map[i][j] === 0) {
-        map[i][j] = 1;
+      if (field[i][j] === 0) {
+        field[i][j] = 1;
         backtracking(cnt + 1);
-        map[i][j] = 0;
+        field[i][j] = 0;
       }
     }
   }
