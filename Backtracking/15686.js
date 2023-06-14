9c2ac65950366a418af8b6c236e22f9447d72cba
aemaaeng <haemin120@naver.com>
2023-06-14 15:10:52 +0900

    :memo: 치킨 배달 - 3회차 풀이

--- Backtracking/15686.js
+++ Backtracking/15686.js
@@ -6,7 +6,6 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [N, M] = input[0].split(" ").map(Number);
 const city = input.slice(1).map((el) => el.split(" ").map(Number));
 
-// 집의 위치와 치킨집의 위치 저장
 const houses = [];
 const chicken = [];
 
@@ -17,33 +16,28 @@ for (let i = 0; i < N; i++) {
   }
 }
 
-let min = Infinity;
-const check = new Array(chicken.length).fill(false);
-
-// 도시의 치킨 거리를 구하는 함수
 function chickenDistance() {
   let distance = 0;
-  // 집에서 각 치킨집까지의 거리를 전부 구함
+  // 도시의 치킨 거리 합을 리턴하는 함수
   for (let i = 0; i < houses.length; i++) {
     let min = Infinity;
     for (let j = 0; j < chicken.length; j++) {
-      // 한 집 당 치킨집까지의 거리
-      // 그 치킨집은 check에서 true여야 한다
-      if (check[j]) {
-        min = Math.min(
-          min,
-          Math.abs(houses[i][0] - chicken[j][0]) +
-            Math.abs(houses[i][1] - chicken[j][1])
-        );
-      }
+      if (!check[j]) continue;
+      min = Math.min(
+        min,
+        Math.abs(houses[i][0] - chicken[j][0]) +
+          Math.abs(houses[i][1] - chicken[j][1])
+      );
     }
     distance += min;
   }
-  // 도시의 치킨 거리 리턴
+
   return distance;
 }
 
-// 백트래킹을 수행하는 함수
+let min = Infinity;
+const check = new Array(chicken.length).fill(false);
+
 function backtracking(idx, cnt) {
   if (cnt === M) {
     if (chickenDistance() < min) min = chickenDistance();
@@ -58,5 +52,4 @@ function backtracking(idx, cnt) {
 }
 
 backtracking(0, 0);
-// 최종 답안 출력
 console.log(min);
