a91f97fae0b53d9366cd5f0c971dd67c41e54536
aemaaeng <haemin120@naver.com>
2023-06-15 16:56:40 +0900

    :memo: 치즈 - 3회차 풀이

--- Graph/2638.js
+++ Graph/2638.js
@@ -7,14 +7,9 @@ const [N, M] = input.shift().split(" ").map(Number);
 const grid = input.map((el) => el.split(" ").map(Number));
 
 let visitedCoords = {};
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
 
-let willMelt = []; // 곧 녹을 치즈 조각을 담은 배열
-let answer = 0;
-let cnt = 0;
-
-// 치즈의 좌표를 미리 저장해둔 객체
 const cheese = {};
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) {
@@ -22,20 +17,17 @@ for (let i = 0; i < N; i++) {
   }
 }
 
-// 외부 공기와 내부 공기를 분리해서 생각해야 한다
-// 외부 공기 = 2, 내부 공기 = 0, 치즈 = 1
-
-// 외부 공기는 전부 2로 바꾸는 bfs
-function checkOutside(x, y) {
+function outsideAir(x, y) {
   const queue = [[x, y]];
+
   while (queue.length) {
-    const [a, b] = queue.shift();
+    const coords = queue.shift();
     for (let i = 0; i < 4; i++) {
-      let nx = dx[i] + a;
-      let ny = dy[i] + b;
+      const nx = coords[0] + dx[i];
+      const ny = coords[1] + dy[i];
 
       if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-      if (!visitedCoords[[nx, ny]] && grid[nx][ny] !== 1) {
+      if (grid[nx][ny] !== 1 && !visitedCoords[[nx, ny]]) {
         visitedCoords[[nx, ny]] = true;
         grid[nx][ny] = 2;
         queue.push([nx, ny]);
@@ -44,59 +36,57 @@ function checkOutside(x, y) {
   }
 }
 
-// 녹을 치즈를 찾는 bfs
-// 4변 중 2변 이상 외부 공기와 인접해야 녹는다
-function checkCheese(x, y) {
-  const queue = [[x, y]];
-  visitedCoords[[x, y]] = true;
-
-  while (queue.length) {
-    const [a, b] = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      let nx = dx[i] + a;
-      let ny = dy[i] + b;
-
-      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-      if (grid[a][b] === 1 && grid[nx][ny] === 2) cnt += 1;
-
-      if (!visitedCoords[[nx, ny]] && grid[nx][ny] === 1) {
-        visitedCoords[[nx, ny]] = true;
-        queue.push([nx, ny]);
-      }
-    }
-
-    if (cnt >= 2) willMelt.push([a, b]);
-    cnt = 0;
-  }
-}
-
-// 치즈를 녹여보는 함수
-function melt(arr) {
+function meltCheese(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let [x, y] = arr[i];
+    const [x, y] = arr[i];
     grid[x][y] = 2;
 
     delete cheese[`${x},${y}`];
   }
 }
 
+let willMelt = [];
+
+function bfs(x, y) {
+  visitedCoords[[x, y]] = true;
+  const queue = [[x, y]];
+  let cnt = 0;
+
+  while (queue.length) {
+    const coords = queue.shift();
+    for (let i = 0; i < 4; i++) {
+      const nx = coords[0] + dx[i];
+      const ny = coords[1] + dy[i];
+
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+      if (grid[x][y] === 1 && grid[nx][ny] === 2) cnt += 1;
+
+      if (!visitedCoords[[nx, ny]] && grid[nx][ny] === 1) {
+        visitedCoords[[nx, ny]] = true;
+        queue.push([nx, ny]);
+      }
+    }
+    if (cnt >= 2) willMelt.push([coords[0], coords[1]]);
+    cnt = 0;
+  }
+}
+
+let hour = 0;
+
 while (Object.keys(cheese).length !== 0) {
-  checkOutside(0, 0);
+  outsideAir(0, 0);
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      if (!visitedCoords[[i, j]] && grid[i][j] === 1) {
-        checkCheese(i, j);
-      }
+      if (grid[i][j] === 1 && !visitedCoords[[i, j]]) bfs(i, j);
     }
   }
 
-  melt(willMelt);
-  answer += 1;
+  meltCheese(willMelt);
+  hour += 1;
 
   willMelt = [];
-  visitedCoords = {};
+  visitedCoords = [];
 }
 
-console.log(answer);
+console.log(hour);
