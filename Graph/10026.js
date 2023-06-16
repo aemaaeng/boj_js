d58d19962dbc507ddf326ada5c5931a19ee37251
aemaaeng <haemin120@naver.com>
2023-06-16 13:41:24 +0900

    :memo: 적록색약 - 3회차 풀이

--- Graph/10026.js
+++ Graph/10026.js
@@ -9,25 +9,23 @@ const arr = input.slice(1).map((el) => el.split(""));
 const dx = [0, 0, -1, 1];
 const dy = [-1, 1, 0, 0];
 
-const answer1 = [];
-const answer2 = [];
+const notBlind = [];
+const blind = [];
 
 let visitedCoords = {};
 let area = 0;
 
-// dfs로 영역 개수 세기 (색약 X)
 function dfs1(x, y) {
   if (visitedCoords[[x, y]]) return;
   visitedCoords[[x, y]] = true;
   area += 1;
 
-  // 주변 탐색
   for (let i = 0; i < 4; i++) {
     const nx = x + dx[i];
     const ny = y + dy[i];
 
     if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-    if (arr[x][y] === arr[nx][ny]) {
+    if (arr[nx][ny] === arr[x][y]) {
       dfs1(nx, ny);
     }
   }
@@ -35,19 +33,23 @@ function dfs1(x, y) {
   return area;
 }
 
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (!visitedCoords[[i, j]]) notBlind.push(dfs1(i, j));
+    area = 0;
+  }
+}
+
 function dfs2(x, y) {
   if (visitedCoords[[x, y]]) return;
   visitedCoords[[x, y]] = true;
   area += 1;
 
-  // 주변 탐색
   for (let i = 0; i < 4; i++) {
     const nx = x + dx[i];
     const ny = y + dy[i];
 
     if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-
-    // R과 G를 같은 취급해야 함.
     if (
       (arr[x][y] === "G" && arr[nx][ny] === "R") ||
       (arr[x][y] === "R" && arr[nx][ny] === "G") ||
@@ -60,26 +62,13 @@ function dfs2(x, y) {
   return area;
 }
 
-// 반복문으로 탐색 - 일반
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (!visitedCoords[[i, j]]) {
-      answer1.push(dfs1(i, j));
-      area = 0;
-    }
-  }
-}
-
 visitedCoords = {};
 
-// 반복문으로 탐색 - 적록색약
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (!visitedCoords[[i, j]]) {
-      answer2.push(dfs2(i, j));
-      area = 0;
-    }
+    if (!visitedCoords[[i, j]]) blind.push(dfs2(i, j));
+    area = 0;
   }
 }
 
-console.log(`${answer1.length} ${answer2.length}`);
+console.log(`${notBlind.length} ${blind.length}`);
