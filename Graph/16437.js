5a77fac2d72c6fc7234fa471504b3e7495ed6b5c
aemaaeng <haemin120@naver.com>
2023-06-18 21:16:44 +0900

    :memo: 양 구출작전 - 3회차 풀이

--- Graph/16437.js
+++ Graph/16437.js
@@ -6,32 +6,30 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = Number(input.shift());
 const islands = input.map((el) => el.split(" "));
 
-const islandInfo = [[], [0, 0]]; // 섬에 어떤 동물이 몇 마리 살고 있는지 담은 배열
-const tree = Array.from(new Array(N + 1), () => new Array().fill([])); // 각 섬의 관계를 트리 자료구조로 표현
+const info = [[], [0, 0]];
+const tree = Array.from(new Array(N + 1), () => new Array().fill([]));
 
-function dfs(x) {
+islands.forEach((el, idx) => {
+  const [t, a, p] = el;
+  info.push([t, Number(a)]);
+  tree[Number(p)].push(idx + 2);
+});
+
+function dfs(v) {
   let cnt = 0;
-  // 재귀
-  for (let i of tree[x]) {
-    cnt += dfs(i);
-  }
 
-  // tree에서 dfs를 돌고 s이면 더하고 w이면 뺀다
-  if (islandInfo[x][0] === "W") {
-    cnt -= islandInfo[x][1];
+  tree[v].forEach((el) => {
+    cnt += dfs(el);
+  });
+
+  if (info[v][0] === "S") {
+    cnt += Number(info[v][1]);
+  } else if (info[v][0] === "W") {
+    cnt -= Number(info[v][1]);
     if (cnt < 0) cnt = 0;
-  } else {
-    cnt += islandInfo[x][1];
   }
 
   return cnt;
 }
 
-islands.forEach((el, idx) => {
-  const [t, a, p] = el;
-  islandInfo.push([t, Number(a)]);
-  tree[Number(p)].push(idx + 2);
-});
-
-// 뿌리는 1번 섬, 1번 섬을 시작점으로 dfs를 돈다
 console.log(dfs(1));
