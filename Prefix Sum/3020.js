f5db69c59b674b6e3b691f9578cfc3dc13baab47
aemaaeng <haemin120@naver.com>
2023-06-20 19:12:03 +0900

    :memo: 개똥벌레 - 3회차 풀이

--- Prefix Sum/3020.js
+++ Prefix Sum/3020.js
@@ -7,7 +7,6 @@ const [N, H] = input.shift().split(" ").map(Number);
 const bottom = new Array(H + 1).fill(0);
 const top = new Array(H + 1).fill(0);
 
-// bottom, top 배열에 각 높이가 등장한 횟수를 저장한다
 input.forEach((el, idx) => {
   idx % 2 === 0 ? (bottom[Number(el)] += 1) : (top[H - Number(el) + 1] += 1);
 });
@@ -17,15 +16,13 @@ for (let i = 1; i <= H; i++) {
   bottom[H - i] += bottom[H - i + 1];
 }
 
-// console.log(bottom);
-// console.log(top);
-
-let min = Infinity;
 let cnt = 0;
+let min = Infinity;
+
 for (let i = 1; i <= H; i++) {
   if (top[i] + bottom[i] < min) {
-    cnt = 1;
     min = top[i] + bottom[i];
+    cnt = 1;
   } else if (top[i] + bottom[i] === min) {
     cnt += 1;
   }
@@ -33,4 +30,8 @@ for (let i = 1; i <= H; i++) {
 
 console.log(min, cnt);
 
-// 참고: https://koguri.tistory.com/111
+// 참고: https://koguri.tistory.com/111 , https://hyeo-noo.tistory.com/310
+// 각 높이가 등장한 횟수를 top, bottom 배열에 저장한다
+// top, bottom 배열에서 누적합을 구한다
+// 두 누적합 배열의 값을 각각 합하게 되면 개똥벌레가 해당 높이에서 직선으로 날았을 때 만나는 장애물의 수가 구해진다.
+// (인덱스 1부터) 두 배열의 각 인덱스끼리 합한 값을 비교해보고 최솟값이 갱신될 때마다 cnt를 초기화하는 방식으로 답을 구할 수 있다.
