f8c6d871c6b915f9773e7e89688a392af3902739
aemaaeng <haemin120@naver.com>
2023-08-14 15:32:52 +0900

    :memo: 미세먼지 안녕! - 풀이 완료

--- Implementation/17144.js
+++ Implementation/17144.js
@@ -39,54 +39,60 @@ function spreadDust(x, y) {
   room[x][y] = cur - spread * cnt;
 }
 
-function rotateClockwise(matrix) {
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-
-  for (let i = 0; i < rows; i++) {
-    if (i < rows - 1) {
-      if (matrix[i + 1][0] === -1) {
-        matrix[i].unshift(0);
-        continue;
-      }
-      let shifted = matrix[i + 1].shift();
-      if (matrix[i][0] === -1) {
-        matrix[i].splice(i + 1, 0, 0);
-        continue;
-      }
-      matrix[i].unshift(shifted);
-    }
-  }
-
-  for (let i = rows - 1; i > 0; i--) {
-    let popped = matrix[i - 1].pop();
-    matrix[i].push(popped);
-  }
-
-  return matrix;
-}
-
 function rotateAntiClockwise(matrix) {
   const rows = matrix.length;
   const cols = matrix[0].length;
 
-  for (let i = 0; i < rows - 1; i++) {
-    let popped = matrix[i + 1].pop();
-    matrix[i].push(popped);
+  // 맨 앞쪽 (열)
+  for (let i = rows - 2; i > 0; i--) {
+    matrix[i][0] = matrix[i - 1][0];
   }
 
-  for (let i = rows - 1; i > 0; i--) {
-    if (matrix[i][0] === -1) {
-      matrix[i].splice(1, 0, 0);
-      continue;
-    }
-    let shifted = matrix[i - 1].shift();
-    if (matrix[i + 1][0] === -1) {
-      matrix[i].unshift();
-      continue;
-    }
-    matrix[i].unshift(shifted);
+  // 맨 윗줄
+  for (let i = 1; i < cols; i++) {
+    matrix[0][i - 1] = matrix[0][i];
   }
+
+  // 맨 끝쪽 (열)
+  for (let i = 0; i < rows - 1; i++) {
+    matrix[i][cols - 1] = matrix[i + 1][cols - 1];
+  }
+
+  // 맨 아랫줄
+  for (let i = cols - 1; i > 1; i--) {
+    matrix[rows - 1][i] = matrix[rows - 1][i - 1];
+  }
+
+  matrix[rows - 1][1] = 0;
+
+  return matrix;
+}
+
+function rotateClockwise(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  // 맨 앞줄 (열)
+  for (let i = 1; i < rows - 1; i++) {
+    matrix[i][0] = matrix[i + 1][0];
+  }
+
+  // 맨 아랫줄
+  for (let i = 1; i < cols; i++) {
+    matrix[rows - 1][i - 1] = matrix[rows - 1][i];
+  }
+
+  // 맨 끝줄 (열)
+  for (let i = rows - 1; i >= 1; i--) {
+    matrix[i][cols - 1] = matrix[i - 1][cols - 1];
+  }
+
+  // 맨 윗줄
+  for (let i = cols - 1; i > 1; i--) {
+    matrix[0][i] = matrix[0][i - 1];
+  }
+
+  matrix[0][1] = 0;
 
   return matrix;
 }
@@ -94,7 +100,6 @@ function rotateAntiClockwise(matrix) {
 let sec = 0;
 
 while (sec < T) {
-  // -- 이 과정이 T초동안 반복되어야 한다@! --
   // 1. 먼지 퍼트리기
   for (let i = 0; i < R; i++) {
     for (let j = 0; j < C; j++) {
@@ -125,7 +130,11 @@ while (sec < T) {
 let result = 0;
 
 room.forEach((row) => {
-  result += row.reduce((acc, cur) => acc + cur);
+  result += row.reduce((acc, cur) => acc + cur, 0);
 });
 
-console.log(result - 2);
+console.log(result + 2);
+
+// 중요한 포인트
+// 먼지를 동시다발적으로 퍼트리는 것
+// 시계방향, 반시계방향 테두리 회전 알고리즘 숙지
