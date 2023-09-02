a1fa308e027b28acd50ba76e62b81a567f8a0f2c
aemaaeng <haemin120@naver.com>
2023-09-02 17:27:25 +0900

    :memo: 게임 개발 - 복습

--- Graph/1516.js
+++ Graph/1516.js
@@ -1,4 +1,4 @@
-// 게임 개발
+// 게임 개발 - 위상정렬
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -7,37 +7,9 @@ const N = Number(input.shift());
 input = input.map((el) => el.split(" ").map(Number));
 input.unshift([]);
 
-function topologicalSort(graph) {
-  const result = time.slice();
-  const queue = [];
-
-  for (let i = 1; i < inDegree.length; i++) {
-    if (inDegree[i] === 0) {
-      queue.push(i);
-    }
-  }
-
-  while (queue.length > 0) {
-    const cur = queue.shift();
-
-    for (const neighbor of graph[cur]) {
-      result[neighbor] = Math.max(
-        result[neighbor],
-        result[cur] + time[neighbor]
-      );
-      inDegree[neighbor] -= 1;
-      if (inDegree[neighbor] === 0) {
-        queue.push(neighbor);
-      }
-    }
-  }
-
-  return result;
-}
-
-const graph = Array.from(new Array(N + 1), () => new Array());
-const inDegree = new Array(graph.length).fill(0);
+const inDegree = new Array(N + 1).fill(0);
 const time = new Array(N + 1).fill(0);
+const graph = Array.from(new Array(N + 1), () => new Array());
 
 for (let i = 1; i < input.length; i++) {
   time[i] = input[i][0];
@@ -47,6 +19,29 @@ for (let i = 1; i < input.length; i++) {
   }
 }
 
-let order = topologicalSort(graph);
-order.shift();
-console.log(order.join("\n"));
+const result = [...time];
+
+function topological_sort() {
+  const queue = [];
+  for (let i = 1; i < inDegree.length; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
+
+  while (queue.length) {
+    const cur = queue.shift();
+    for (const neighbor of graph[cur]) {
+      result[neighbor] = Math.max(
+        result[neighbor],
+        result[cur] + time[neighbor]
+      );
+      inDegree[neighbor] -= 1;
+      if (inDegree[neighbor] === 0) queue.push(neighbor);
+    }
+  }
+
+  for (let i = 1; i < result.length; i++) {
+    console.log(result[i]);
+  }
+}
+
+topological_sort();
